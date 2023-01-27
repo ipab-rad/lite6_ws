23c08e7e190fd478cb55c6818c90aec1a22e2428
peterdavidfagan <peterdavidfagan@gmail.com>
2023-01-27 15:24:16 +0000

    move copy of ros_entrypoint

--- .docker/ros_entrypoint.sh
+++ .docker/ros_entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 # setup ros2 environment
-source "/opt/ros/${ROS_DISTRO}/setup.bash" --
-source "/root/ros2_ws/install/local_setup.bash" --
+source "/opt/ros/$ROS_DISTRO/setup.bash" --
+source "$ROS_UNDERLAY/setup.bash" --
